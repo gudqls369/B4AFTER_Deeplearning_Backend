10f7fd3911e770aabe23c57f7d341e92daee21aa
kimmyeongji <myeongji0888@gmail.com>
2022-11-23 09:21:10 +0900

    Feat Login

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import ( #simplejwt 기본 기능 import, si
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserView
+from .views import UserView, mockView
  
 urlpatterns = [
     path('signup/', UserView.as_view(), name='user_view'), #회원가입 URL
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #simplejwt 기본 기능
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #simplejwt 기본 기능
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #token key 발급받기 위한 API    
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #refresh key 발급받기 위한 API    
+    path('mock/', mockView.as_view(), name='mock _view'), #로그인 기능 테스트
+
 ]
